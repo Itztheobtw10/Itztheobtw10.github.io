21267099834acc55dd6c52ad50866d0ae95e4075
Itztheobtw <theoadeniji7@gmail.com>
2025-05-19 20:16:48 +0100

    lil update

--- cs practice python/volume.py
+++ cs practice python/volume.py
@@ -1,10 +1,67 @@
-import math
+'''try:
+    n = int(input("Enter a number: "))
+    total = 0
 
-a = float(input("Please Enter Length A"))
-b = float(input("Please Enter Length B"))
-c = float(input("Please Enter Length C"))
+    for i in range(1, n):
+        total += i
 
-s = (a+b+c) / 2
+    print(total)
+except ValueError:
+    print("Please enter a valid number!")'''
 
-area = math.sqrt(s(s-a)(s-b)(s-c))
-print(round("Your Area of the Triangle with the given length values is:",area),1)
+'''value = int(input())
+count = 0 
+
+for item in numbers:
+    if item == value:
+        count += 1
+    
+print(count)'''
+
+'''word = input("Enter your word: ")
+rev = ""
+
+for i in range(len(word)-1,-1,-1):
+    rev = rev + word[i]
+    
+print(rev)'''
+
+'''numbers = [1,2,3,4,5,6,7,8]
+sum = 0 
+for i in numbers:
+    if i % 2 == 0:
+        sum += i
+print(sum)
+
+largest = numbers[0]  
+smallest = numbers[-1]    
+for z in numbers:
+    if z > largest:
+        largest = z
+    if z < smallest:
+        smallest = z
+print(largest)
+print(smallest)
+
+text = input()
+occ = 0 
+for x in text:
+    if x.isupper():
+        occ += 1
+print(f"There are {occ} numbers of Capital Letters")'''
+word = input("Enter a word: ")
+rle_list = []
+count = 1
+
+for i in range(1, len(word)):
+    if word[i] == word[i - 1]:
+        count += 1
+    else:
+        rle_list.append((word[i - 1], count))
+        count = 1 
+
+
+rle_list.append((word[-1], count))
+
+res = str(rle_list)[1:-1].replace("'", '').upper()
+print(res)
